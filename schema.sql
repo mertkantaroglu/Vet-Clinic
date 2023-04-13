dcbc3d56ffc1da9c1677d985bafafb64d84d3287
Mert Kantaroglu <mertkantaroglu@gmail.com>
2023-04-13 13:43:38 +0300

    Create vets, specializations and visits schemas

--- schema.sql
+++ schema.sql
@@ -19,3 +19,23 @@ CREATE TABLE species (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255)
 );
+
+CREATE TABLE vets (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  age INTEGER,
+  date_of_graduation DATE
+);
+
+CREATE TABLE specializations (
+  vet_id INTEGER REFERENCES vets(id),
+  species_id INTEGER REFERENCES species(id),
+  PRIMARY KEY (vet_id, species_id)
+);
+
+CREATE TABLE visits (
+  id SERIAL PRIMARY KEY,
+  animal_id INTEGER REFERENCES animals(id),
+  vet_id INTEGER REFERENCES vets(id),
+  visit_date DATE
+);
