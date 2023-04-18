a97f117c6401d8e4e0d3ab088f0523391e4ff5a3
Mert Kantaroglu <mertkantaroglu@gmail.com>
2023-04-18 17:15:55 +0300

    Fix PR issues

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -24,3 +24,29 @@ CREATE TABLE treatments (
   type VARCHAR(50),
   name VARCHAR(255),
 );
+
+CREATE TABLE invoice_items (
+  id SERIAL PRIMARY KEY,
+  unit_price DECIMAL,
+  quantity INT,
+  total_price DECIMAL,
+  invoice_id INT REFERENCES invoices(id),
+  treatment_id INT REFERENCES treatments(id)
+);
+
+CREATE TABLE medical_histories_treatments (
+  id SERIAL PRIMARY KEY,
+  medical_history_id INT NOT NULL,
+  treatment_id INT NOT NULL,
+  CONSTRAINT fk_medical_histories_treatments_medical_histories
+    FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id),
+  CONSTRAINT fk_medical_histories_treatments_treatments
+    FOREIGN KEY (treatment_id) REFERENCES treatments(id)
+);
+
+CREATE INDEX patient_index ON medical_histories(patient_id);
+CREATE INDEX treatment_index ON invoice_items(treatment_id);
+CREATE INDEX medical_history_index ON invoices(medical_history_id);
+CREATE INDEX invoice_index ON invoice_items(invoice_id);
+CREATE INDEX medical_histories_treatments_medical_history_id_index ON medical_histories_treatments (medical_history_id);
+CREATE INDEX medical_histories_treatments_treatment_id_index ON medical_histories_treatments (treatment_id);
